cf6bcd21b02d160b9ea4cd26b89f4d38b9a98874
kunzhutich <131626756+kunzhutich@users.noreply.github.com>
2023-11-06 19:04:12 -0800

    populating tables

---
+++ populate_stadium.sql
@@ -0,0 +1,5 @@
+INSERT INTO stadium (st_name, st_size, st_location)
+
+VALUES
+
+()
---
+++ populate_teams.sql
@@ -0,0 +1,5 @@
+INSERT INTO team (t_tname, t_foundyear, t_city, t_cid, t_manager, t_stid)
+
+VALUES
+
+('Sacramento Kings', 1923, 'Sacramento', )
