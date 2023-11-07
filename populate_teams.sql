78634f35b1629f3590630868ae693210023f8bad
kunzhutich <131626756+kunzhutich@users.noreply.github.com>
2023-11-06 21:02:07 -0800

    Update populate_teams.sql

--- populate_teams.sql
+++ populate_teams.sql
@@ -1,5 +1,34 @@
-INSERT INTO team (t_tname, t_foundyear, t_city, t_cid, t_manager, t_stid)
+INSERT INTO team (t_tname, t_foundyear, t_city, t_coachid, t_manager, t_stid)
 
 VALUES
 
-('Sacramento Kings', 1923, 'Sacramento', )
+('Sacramento Kings', 1985, 'Sacramento', --Mike Brown--, , --Golden 1 Center--), 1923
+('Golden State Warriors', 1971, 'San Francisco', --Steve Kerr--, , --Chase Center--), 1946
+('Atlanta Hawks', 1968, 'Atlanta', --Quin Snyder--, , --State Farm Arena--), 1946
+('Boston Celtics', 1946, 'Boston', --Joe Mazzulla--, , --TD Garden--),
+('Oklahoma City Thunder', 2008, 'Oklahoma City', --Mark Daigneault--, , --Paycom Center--), 1967
+('Los Angeles Lakers', 1960, 'Los Angeles', --Darvin Ham--, , --Crypto.com Arena--), 1947
+('Utah Jazz', 1979, 'Salt Lake City', --Will Hardy--, , --Vivint Arena--), 1974
+('Memphis Grizzlies', 2001, 'Memphis', --Taylor Jenkins--, , --FedEx Forum--), 1995
+('Milwaukee Bucks', 1968, 'Milwaukee', --Mike Budenholzer--, , --Fiserv Forum--),
+('Indiana Pacers', 1976, 'Indianapolis', --Rick Carlisle--, , --Gainbridge Fieldhouse--), 1967
+('New York Knicks', 1946, 'New York', --Tom Thibodeau--, , --Madison Square Garden--),
+('Denver Nuggets', 1976, 'Denver', --Michael Malone--, , --Ball Arena--), 1967
+('Minnesota Timberwolves', 1989, 'Minneapolis', --Chris Finch--, , --Target Center--),
+('Philadelphia 76ers', 1963, 'Philadelphia', --Doc Rivers--, , --Wells Fargo Center--), 1946
+('New Orleans Pelicans', 2013, 'New Orleans', --Willie Green--, , --Smoothie King Center--), 2002
+('Dallas Mavericks', 1980, 'Dallas', --Jason Kidd--, , --American Airlines Center--),
+('Phoenix Suns', 1968, 'Phoenix', --Monty Williams--, , --Footprint Center--),
+('Los Angeles Clippers', 1984, 'Los Angeles', --Tyronn Lue--, , --Crypto.com Arena--), 1970
+('Portland Trail Blazers', 1970, 'Portland', --Chauncey Billups--, , --Moda Center--),
+('Brooklyn Nets', 2012, 'Brooklyn', --Jacque Vaughn--, , --Barclays Center--), 1967
+('Washington Wizards', 1997, 'Washington', --Wes Unseld--, , --Capital One Arena--), 1961
+('Chicago Bulls', 1966, 'Chicago', --Billy Donovan--, , --United Center--),
+('San Antonio Spurs', 1976, 'San Antonio', --Gregg Popovich--, , --Frost Bank Center--), 1967
+('Toronto Raptors', 1995, 'Toronto', --Nick Nurse--, , --Scotiabank Arena--),
+('Cleveland Cavaliers', 1970, 'Cleveland', --J.B. Bickerstaff--, , --Rocket Mortgage Fieldhouse--),
+('Orlando Magic', 1989, 'Orlando', --Jamahl Mosley--, , --Amway Center--),
+('Charlotte Hornets', 1988, 'Charlotte', --Steve Clifford--, , --Spectrum Center--),
+('Houston Rockets', 1971, 'Houston', --Stephen Silas--, , --Toyota Center--), 1967
+('Detroit Pistons', 1957, 'Detroit', --Dwane Casey--, , --Little Caesars Arena--), 1937
+('Miami Heat', 1988, 'Miami', --Erik Spoelstra--, , --Kaseya Center--)
