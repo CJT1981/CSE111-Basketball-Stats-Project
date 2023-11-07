804b18e58e78f2eced0bd33541bf3ed4b0e84a94
kunzhutich <131626756+kunzhutich@users.noreply.github.com>
2023-11-06 19:03:38 -0800

    Update easy_tables.sql

--- easy_tables.sql
+++ easy_tables.sql
@@ -1,21 +1,23 @@
 CREATE TABLE coaches (
-    c_cid identity(1, 1) primary key,
+    c_coachid identity(1, 1) primary key,
     c_name varchar(50),
     c_startyear date not null,
     c_numofchamp int
 )
 
 CREATE TABLE stadium (
-    st_stname identity(1, 1) primary key,
+    st_stadiumid identity(1, 1) primary key,
+    st_name varchar(50),
     st_size int,
     st_location varchar(50)
 )
 
 CREATE TABLE team (
-    t_tname identity(1, 1) primary key,
+    t_teamid identity(1, 1) primary key,
+    t_tname varchar(50),
     t_foundyear date not null,
     t_city varchar(50),
     t_cid int, 
     t_manager varchar(50),
-    t_stname int
+    t_stid int
 )
