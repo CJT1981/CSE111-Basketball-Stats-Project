85afdc246320c688218ed88797b5ff83c2033cf9
kunzhutich <61340635+kunzhutich@users.noreply.github.com>
2023-11-07 11:16:49 -0800

    Create update_stadium.sql

---
+++ update_stadium.sql
@@ -0,0 +1,119 @@
+UPDATE stadium
+SET st_size = 17608
+WHERE st_stadiumid = 26;
+
+UPDATE stadium
+SET st_size = 18064
+WHERE st_stadiumid = 10;
+
+UPDATE stadium
+SET st_size = 16600
+WHERE st_stadiumid = 1;
+
+UPDATE stadium
+SET st_size = 19156
+WHERE st_stadiumid = 2;
+
+UPDATE stadium
+SET st_size = 18203
+WHERE st_stadiumid = 21;
+
+UPDATE stadium
+SET st_size = 18306
+WHERE st_stadiumid = 29;
+
+UPDATE stadium
+SET st_size = 17794
+WHERE st_stadiumid = 15;
+
+UPDATE stadium
+SET st_size = 17385
+WHERE st_stadiumid = 17;
+
+UPDATE stadium
+SET st_size = 17274
+WHERE st_stadiumid = 12;
+
+UPDATE stadium
+SET st_size = 19812
+WHERE st_stadiumid = 20;
+
+UPDATE stadium
+SET st_size = 19520
+WHERE st_stadiumid = 8;
+
+UPDATE stadium
+SET st_size = 18798
+WHERE st_stadiumid = 18;
+
+UPDATE stadium
+SET st_size = 21000
+WHERE st_stadiumid = 23;
+
+UPDATE stadium
+SET st_size = 16867
+WHERE st_stadiumid = 19;
+
+UPDATE stadium
+SET st_size = 21146
+WHERE st_stadiumid = 7;
+
+UPDATE stadium
+SET st_size = 17071
+WHERE st_stadiumid = 24;
+
+UPDATE stadium
+SET st_size = 19079
+WHERE st_stadiumid = 13;
+
+UPDATE stadium
+SET st_size = 19393
+WHERE st_stadiumid = 25;
+
+UPDATE stadium
+SET st_size = 17732
+WHERE st_stadiumid = 3;
+
+UPDATE stadium
+SET st_size = 20356
+WHERE st_stadiumid = 30;
+
+UPDATE stadium
+SET st_size = 20917
+WHERE st_stadiumid = 4;
+
+UPDATE stadium
+SET st_size = 18418
+WHERE st_stadiumid = 27;
+
+UPDATE stadium
+SET st_size = 20511
+WHERE st_stadiumid = 28;
+
+UPDATE stadium
+SET st_size = 19432
+WHERE st_stadiumid = 6;
+
+UPDATE stadium
+SET st_size = 18846
+WHERE st_stadiumid = 22;
+
+UPDATE stadium
+SET st_size = 20200
+WHERE st_stadiumid = 5;
+
+UPDATE stadium
+SET st_size = 18104
+WHERE st_stadiumid = 11;
+
+UPDATE stadium
+SET st_size = 20332
+WHERE st_stadiumid = 9;
+
+UPDATE stadium
+SET st_size = 19600
+WHERE st_stadiumid = 16;
+
+DELETE
+FROM stadium
+WHERE st_stadiumid = 14;
